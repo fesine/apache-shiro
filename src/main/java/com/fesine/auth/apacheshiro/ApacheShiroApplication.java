4429d020031c0d29980477aa714d4f5899ef77e0
Fesine <240944773@qq.com>
2018-01-10 15:21:54 +0800

    Initial commit

---
+++ src/main/java/com/fesine/auth/apacheshiro/ApacheShiroApplication.java
@@ -0,0 +1,12 @@
+package com.fesine.auth.apacheshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheShiroApplication.class, args);
+	}
+}
